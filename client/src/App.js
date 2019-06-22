234d1b39bd8d78bfbe9835644929bbea73cc7611
Jing <jingkcheng@hotmail.com>
2019-06-22 00:48:24 -0400

    Scroll to top added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import HeaderIntro from './components/HeaderIntro';
 import MainTeam from './components/MainTeam';
 import ArtifactCard from './components/MainArtifacts';
 import FooterPage from './components/FooterBar';
+import ScrollUpButton from "react-scroll-up-button";
 import { Container } from 'shards-react';
 
 import { Provider } from 'react-redux';
@@ -18,20 +19,21 @@ class App extends Component {
   render(){
     return (
       <Provider store={store}>
-      <div className="App" align="center">
-        <AppNavBar/>
-        <HeaderIntro/>
-        <MainTeam/>
-        <ArtifactCard/>
-      </div>
-      <div className="divider"></div>
-      <div className="character-list-background">
-        <Container>
-          <CharacterModal/>
-          <CharacterList/>
-        </Container>
-      </div>
-        <FooterPage/>
+        <div className="App" align="center">
+          <AppNavBar/>
+          <HeaderIntro/>
+          <MainTeam/>
+          <ArtifactCard/>
+        </div>
+        <div className="divider"></div>
+        <div className="character-list-background">
+          <Container>
+            <CharacterModal/>
+            <CharacterList/>
+          </Container>
+        </div>
+          <ScrollUpButton/>
+          <FooterPage/>
       </Provider>
       );
   }
