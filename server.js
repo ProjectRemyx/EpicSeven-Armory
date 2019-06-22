3e0eb27c27e8d5e7518989ac85d1751f10b7d8e1
Jing <jingkcheng@hotmail.com>
2019-06-21 21:29:20 -0400

    Changed proxy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); //Routing
 const mongoose = require('mongoose'); //ORM
 const bodyParser = require('body-parser'); //Get data from requests
+const path = require('path');
 
 //Set up routes
 const characters = require('./routes/api/characters'); //Character routes
@@ -22,5 +23,17 @@ mongoose
 //Use Routes
 app.use('/api/characters', characters); //For anything going to /api/characters refers to the required file
 
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
